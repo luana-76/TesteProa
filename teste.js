ade8f788cce034810a0f979c8ec3a2dc9e14f6a2
Luana <luana_dark34@hotmail.com>
2024-08-25 12:56:00 -0300

    atividadeDeCasa

---
+++ teste.js
@@ -0,0 +1,86 @@
+/*function soma(numero1 = 0, numero2 = 0){
+
+    return numero1 + numero2;
+
+}
+console.log(soma(numero2 = 8, numero1 = 4));*/
+
+/*function getSize(alt, lar, com){
+
+    let area = alt * lar;
+    let volume = alt * lat * com;
+    let sizes = [area, volume, alt, lar, com];
+    return sizes;
+
+}
+
+console.log(getSize(3, 3, 2));
+console.log(getSize(3, 3, 2)[0]);
+console.log(getSize(3, 3, 2)[1]);*/
+
+/*function soma(num){
+
+    for(let i=1; i < 11;i++){
+
+        console.log(`${num} + ${i} = ${num + i}`)
+    }
+
+}
+soma(2)
+
+console.log()
+
+function multiplicacao(num){
+
+    for(let i=1; i < 11;i++){
+
+        console.log(`${num} x ${i} = ${num * i}`)
+    }
+
+}
+multiplicacao(2)*/
+
+/*function pularNumero(num = 0){
+
+    for(let cont = 0; cont < num;cont++){
+
+        if(cont == 5){
+
+            continue
+
+        }
+        console.log(`O número é ${cont}`)
+
+    }
+
+}
+
+pularNumero(10);*/
+
+/*let numeros = [23, 4, 10, 15];
+let soma = 0;
+function somaArray(array){
+
+    for(let i = 0; i < array.length;i++){
+
+        soma += array[i]
+
+    }
+    console.log(`A soma do array é ${soma}`)
+
+}
+
+somaArray(numeros);*/
+
+function queroLargar(sextou){
+
+    for(let i = 0; i < 29;i++){
+
+        console.log(sextou)
+
+    }
+    
+
+}
+
+queroLargar("Quero largar!")
